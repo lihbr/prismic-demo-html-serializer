e45fd9ae5b2b536575e75626c6888fabf9d137b8
lihbr <lihbr@users.noreply.github.com>
2022-04-13 16:20:40 +0200

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <PrismicRichText
+    v-if="home"
+    :field="home.data.body"
+    class="container mx-auto max-w-2xl py-16 space-y-2"
+  />
+</template>
+
+<script setup lang="ts">
+import { useSinglePrismicDocument } from '@prismicio/vue';
+
+const { data: home } = useSinglePrismicDocument("home")
+</script>

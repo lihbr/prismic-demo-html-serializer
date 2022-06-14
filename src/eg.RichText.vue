571fb0685479b20bd222d57b2fb7cb924494bce7
Lucie <25330882+lihbr@users.noreply.github.com>
2022-06-14 14:09:13 +0200

    chore: update example

--- src/eg.RichText.vue
+++ src/eg.RichText.vue
@@ -2,12 +2,11 @@
   <PrismicRichText
     v-if="home"
     :field="home.data.body"
-    class="container mx-auto max-w-2xl py-16 space-y-2"
   />
 </template>
 
 <script setup lang="ts">
-import { useSinglePrismicDocument } from '@prismicio/vue';
+import { useSinglePrismicDocument, PrismicRichText } from '@prismicio/vue';
 
 const { data: home } = useSinglePrismicDocument('home')
 </script>
